f93f23b4bae056e4900498990b084f48c51942f9
evanderfff123-boop <evanderfff123@gmail.com>
2026-07-02 10:49:26 +0800

    add 20260702: 搜索插入位置+爬楼梯+杨辉三角

---
+++ 0035_搜索插入位置.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            # // 整除运算符，向下取整
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
